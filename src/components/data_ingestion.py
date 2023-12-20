fba89607dcc86022f140da88941ee53b5a692526
Rajesh Mane <rajeshmane711@gmail.com>
2023-12-20 08:15:32 +0530

    Added Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,7 @@ from logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from data_transformation import DataTransformation
 
 @dataclass
 class DataIngestionConfig:
@@ -39,6 +40,17 @@ class DataIngestion:
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=43)
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
             test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+            logging.info("Ingestion of data is completed")
+
+            return (self.ingestion_config.train_data_path, 
+                    self.ingestion_config.test_data_path)
         except:
             pass
+
+if __name__ == '__main__':
+    obj = DataIngestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
 
